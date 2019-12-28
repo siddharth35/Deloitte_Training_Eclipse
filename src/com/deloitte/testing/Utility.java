5b73c5f2745382c9a906f85acf254957a87b588d
Admin <Admin@192.168.22.127>
2019-12-27 20:12:29 -0800

    Commit 1

--- src/com/deloitte/testing/Utility.java
+++ src/com/deloitte/testing/Utility.java
@@ -22,9 +22,11 @@ public class Utility {
 			return "";
 		}
 	}
+
 	public static boolean isPrime(int n) {
-		for(int i = 2; i <= Math.sqrt(n); i++) {
-			if(n%i == 0) return false;
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0)
+				return false;
 		}
 		return true;
 	}
