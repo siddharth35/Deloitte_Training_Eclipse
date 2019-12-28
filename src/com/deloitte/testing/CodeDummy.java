1cc45207a24cf6817dcf3f5a9f72193ccc696c51
Admin <Admin@192.168.22.127>
2019-12-27 20:06:56 -0800

    Commit 1

---
+++ src/com/deloitte/testing/CodeDummy.java
@@ -0,0 +1,60 @@
+package com.deloitte.testing;
+
+public class CodeDummy {
+	public static void main(String[] args) {
+		currencyConverter();
+		series(Integer.parseInt(Utility.read("Enter n: ")));
+	}
+
+/*Currency Converter*/
+	public static void currencyConverter() {
+		boolean run = true;
+		while (run) {
+			menu();
+			String k = new String(Utility.read("Enter Option:"));
+			switch (k) {
+			case "1":
+				usdToInr(Utility.read("Enter Currency in USD: "));
+				break;
+			case "2":
+				inrToUsd(Utility.read("Enter Currency in INR: "));
+				break;
+			case "0":
+				System.out.println("Thank you");
+				run = false;
+				break;
+			default:
+				System.out.println("Invalid Choice");
+			}
+		}
+	}
+
+	private static void menu() {
+		System.out.println("1:INR-USD\n2:USD-INR\n0:EXIT");
+	}
+
+	private static void usdToInr(String k) {
+		try {
+			System.out.printf("\n\n%s USD = %.2f INR\n\n", k, Double.parseDouble(k) * 71.205);
+		} catch (NumberFormatException nfe) {
+			System.out.println("Not a Valid Number");
+		}
+	}
+
+	private static void inrToUsd(String k) {
+		try {
+			System.out.printf("\n\n%s INR = %.2f USD\n\n", k, Double.parseDouble(k) / 71.205);
+		} catch (NumberFormatException nfe) {
+			System.out.println("Not a Valid Number");
+		}
+	}
+/*Series*/
+	public static void series(int n) {
+		String out = "";
+		for (int i = 1; i <= n; i++) {
+			out = i % 2 + " " + out;
+			System.out.println(out + "\n");
+		}
+	}
+
+}
---
+++ src/com/deloitte/testing/Utility.java
@@ -0,0 +1,31 @@
+package com.deloitte.testing;
+
+import java.io.IOException;
+
+public class Utility {
+	private Utility() {
+	}
+
+	public static String read(String message) {
+		try {
+			int k;
+			String out = "";
+			System.out.print(message);
+			do {
+				k = System.in.read();
+				if (k != 10 && k != 13) {
+					out = out + (char) k;
+				}
+			} while (k != 10);
+			return out;
+		} catch (IOException e) {
+			return "";
+		}
+	}
+	public static boolean isPrime(int n) {
+		for(int i = 2; i <= Math.sqrt(n); i++) {
+			if(n%i == 0) return false;
+		}
+		return true;
+	}
+}
